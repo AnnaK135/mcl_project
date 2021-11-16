8eb7913a4c7ba6aa8e483348148ce36f87735865
AnnaK135 <anna.karpukhina12@gmail.com>
2021-11-16 11:05:22 +0100

    Added rose.sh

--- atac_bulc.sh
+++ atac_bulc.sh
@@ -5,3 +5,43 @@
 [ -z $BASH ] || shopt -s expand_aliases
 alias BEGINCOMMENT="if [ ]; then"
 alias ENDCOMMENT="fi"
+
+path_to_project="/mnt/e/MCL_project/atac/"
+path_to_tools="/mnt/e/tools/"
+path_to_ref="/mnt/e/MCL_project/chipseq_nucleolin/ref"
+path_to_raw_fastq="$path_to_project""fastq/"
+path_to_clean_fastq="$path_to_project""fastq_clean/"
+path_to_fastqc="$path_to_project""fastqc/"
+path_to_bwa_files="$path_to_project""bwa/"
+genome_fa="GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
+index="GRCh38_noalt_as.1.bt2"
+srr=(SRR891268 SRR891269 SRR891270 SRR891271)
+
+BEGINCOMMENT
+
+### FastQC quality analysis
+fastqc "$path_to_raw_fastq"* -o "$path_to_fastqc" &
+
+### Removing adapters with NGmerge
+for i in ${srr[*]};do
+    "$path_to_tools"NGmerge/NGmerge -a -1 "$path_to_raw_fastq"${i}_1.fastq -2 "$path_to_raw_fastq"${i}_2.fastq -o "$path_to_clean_fastq"${i} -v -n 10 -u 41 &
+done
+ENDCOMMENT
+
+### FastQC quality analysis of clean reads
+fastqc "$path_to_clean_fastq"* -o "$path_to_fastqc" &
+
+### Alignment with Bowtie, stats and sorting
+for i in ${srr[*]};do
+    bowtie2 --very-sensitive -x "$path_to_ref""$index" -k 10 -1 "$path_to_clean_fastq"${i}_1.fastq - 2 "$path_to_clean_fastq"${i}_clean_2.fastq -p 10 \
+        | samtools view -u - \
+        | samtools sort -n - o "$path_to_bwa_files"${i}_sorted.bam -
+    samtools flagstat "$path_to_bwa_files"${i}_sorted.bam > "$path_to_bwa_files"${i}.mapping_stat
+done
+
+BEGINCOMMENT
+### Peak calling with Genrich
+#(in the ATACseq mode -j, removing duplicates -r and mitochondrial reads -e chrM and for multiple replicates at once)
+"$path_to_tools"Genrich/Genrich -j -r -e chrM
+
+ENDCOMMENT
---
+++ rose.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+### For commenting out pieces of the code ###
+[ -z $BASH ] || shopt -s expand_aliases
+alias BEGINCOMMENT="if [ ]; then"
+alias ENDCOMMENT="fi"
+
+### Introducing variables for paths so we can change them later and reuse the script ###
+path_to_project="/mnt/e/MCL_project/chipseq_h3_ubf/"
+path_to_ref="/mnt/e/MCL_project/ref/"
+path_to_tools="/mnt/e/tools/"
+path_to_bwa_files="$path_to_project""bwa/"
+path_to_rose_files="$path_to_project""rose/"
+
+samples=(L1b L2b L3b Linput G1b G2b G3b Ginput)
+lcl=(L1b L2b L3b)
+granta=(G1b G2b G3b)
+
+BEGINCOMMENT
+### Indexing the BAM files
+for i in ${samples[*]};do
+    samtools index -b -@ 20 "$path_to_bwa_files"${i}_sorted_marked_filtered.bam &
+done
+ENDCOMMENT
+
+### Calling superenhancers with ROSE ### 
+cd "$path_to_tools"rose
+
+for i in ${lcl[*]};do
+    python ROSE_main.py -g hg38 -i "$path_to_rose_files"LCL_H3K27Ac_chipr_all.gff \
+                                -r "$path_to_bwa_files"${i}_sorted_marked_filtered.bam \
+                                -o "$path_to_rose_files"rose_output_${i} -t 2500 \
+                                -c "$path_to_bwa_files"Linput_sorted_marked_duplicates.bam &
+done
+
+for i in ${granta[*]};do
+    python ROSE_main.py -g hg38 -i "$path_to_rose_files"GRANTA_H3K27Ac_chipr_all.gff \
+                                -r "$path_to_bwa_files"${i}_sorted_marked_filtered.bam \
+                                -o "$path_to_rose_files"rose_output_${i} -t 2500 \
+                                -c "$path_to_bwa_files"Ginput_sorted_marked_duplicates.bam &
+done
